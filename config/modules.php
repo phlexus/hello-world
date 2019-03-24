4f262fbceaa528141094e3e1b8845d1314a76e73
Anton Vasiliev <aionbase@gmail.com>
2019-03-24 21:09:09 +0000

    Fixed module key name in config/modules.php

--- config/modules.php
+++ config/modules.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'className' => 'MyApp\Modules\CustomHelloWorld\Module',
-    'path' => __DIR__ . '/../modules/custom-hello-world/Module.php',
-    'router' => __DIR__ . '/../modules/custom-hello-world/config/routes.php',
+    'CustomHelloWorld' => [
+        'className' => 'MyApp\Modules\CustomHelloWorld\Module',
+        'path' => __DIR__ . '/../modules/custom-hello-world/Module.php',
+        'router' => __DIR__ . '/../modules/custom-hello-world/config/routes.php',
+    ],
 ];
